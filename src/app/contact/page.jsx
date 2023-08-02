795a3aadfbc394cf3ea16f0748e2f828c956029a
TechnoGeek01 <rsudharsangames@gmail.com>
2023-08-02 20:27:31 +0530

    finished portfolio and contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,34 @@
 import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/Button/Button";
 
 const Contact = () => {
-  return <div className={styles.contact}>Contact</div>;
+  return (
+    <div className={styles.contact}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.image}
+            src="/contact.png"
+            alt="contact"
+            fill={true}
+          />
+        </div>
+        <form className={styles.form}>
+          <input className={styles.input} type="text" placeholder="name" />
+          <input className={styles.input} type="email" placeholder="name" />
+          <textarea
+            className={styles.textArea}
+            placeholder="message"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,5 +1,51 @@
 import styles from "./page.module.css";
-const Category = () => {
-  return <div className={styles.container}>Category</div>;
+import Image from "next/image";
+import Button from "@/components/Button/Button";
+
+const Category = ({ params }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <p className={styles.title}>Title</p>
+          <h1 className={styles.desc}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores
+            deserunt similique iure. At, autem quia aliquam voluptates et
+            excepturi eum!
+          </h1>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            src="https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZGlnaXRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60"
+            alt=""
+            fill={true}
+          />
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <p className={styles.title}>Title</p>
+          <h1 className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            aperiam possimus, quos harum at nihil voluptates. Nulla dolorem
+            laudantium voluptatem.
+          </h1>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            src="https://images.unsplash.com/photo-1516321318423-f06f85e504b3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZGlnaXRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60"
+            alt=""
+            fill={true}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Category;
